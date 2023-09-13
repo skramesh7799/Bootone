5515bf1fcdc4bf15fb47da0ba725ab3806bbac49
ramesh <skramesh7799@gmail.com>
2023-09-13 21:20:24 +0530

    Thymeleaf commit

---
+++ src/main/java/com/app/raghu/SpringBootThymeleafExOne1Application.java
@@ -0,0 +1,13 @@
+package com.app.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafExOne1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafExOne1Application.class, args);
+	}
+
+}
